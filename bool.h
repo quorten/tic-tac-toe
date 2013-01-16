f6143fe31013d405e2663480b73da12d6be18fca
Andrew Makousky <mako0042@umn.edu>
2013-01-16 07:04:00 -0600

    Save file.

---
+++ bool.h
@@ -0,0 +1,11 @@
+/* This is basically just boolean definitions.  */
+#ifndef BOOL_H
+#define BOOL_H
+
+#ifndef __cplusplus
+/* C Language extensions.  */
+enum bool_tag { false, true };
+typedef enum bool_tag bool;
+#endif
+
+#endif /* not BOOL_H */
